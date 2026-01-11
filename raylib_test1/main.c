4ecc00e296847035870cfd9caa294ddac33fd469
zagdiablo <adhitia.nugrahatkj@gmail.com>
2026-01-11 12:42:25 +0700

    updated raylib_test1

--- raylib_test1/main.c
+++ raylib_test1/main.c
@@ -1,66 +1,132 @@
 #include "raylib.h"
+#include <stdbool.h>
 
 
-typedef struct Box {
-    float width;
-    float height;
-    float x;
-    float y;
-    float dx;
-    float dy;
-    Color color;
-} box_t;
+typedef struct GameState{
+    int score;
+    Rectangle player;
+    Rectangle coin;
+    Rectangle enemy;
+    bool gameOver;
+    int enemy_dx;
+    int enemy_dy;
+} gamestate_t;
+
+
+gamestate_t InitGame(const int *window_width, const int *window_height){
+    gamestate_t initialState = {
+        .score = 0,
+        .player = {
+            .x = 400,
+            .y = 300,
+            .width = 50,
+            .height = 50,
+        },
+        .coin = {
+            .x = GetRandomValue(50, (*window_width) - 50),
+            .y = GetRandomValue(50, (*window_height) - 50),
+            .width = 25,
+            .height = 25,
+        },
+        .enemy = {
+            .x = 0,
+            .y = 0,
+            .width = 40,
+            .height= 40,
+        },
+        .gameOver = false,
+        .enemy_dx = 10,
+        .enemy_dy = 10,
+    };
+
+    return initialState;
+}
 
 
 int main(void){
     const int window_width = 800;
     const int window_height = 600;
-    InitWindow(window_width, window_height, "Dugerio");
+
+    InitWindow(window_width, window_height, "Duggerio");
+    InitAudioDevice();
     SetTargetFPS(60);
 
-    // box object
-    box_t box_1 = {
-        .width = 50,
-        .height = 50,
-        .x = window_width / 2.0f,
-        .y = window_height / 2.0f,
-        .dx = 5.0f,
-        .dy = 5.0f,
-        .color = MAROON,
-    };
+    gamestate_t state = InitGame(&window_width, &window_height);
+    Texture2D player_sprite = LoadTexture("assets/player/player_9mmhandgun.png");
+    Sound coin_sound = LoadSound("assets/sounds/coin-recieved.mp3");
 
-    while (!WindowShouldClose()){
-        box_1.x += box_1.dx;
-        box_1.y += box_1.dy;
+    while (!WindowShouldClose()) {
+        // CPU calculation
+        // enemy movement
+        if(!state.gameOver){
+            state.enemy.x += state.enemy_dx;
+            if(state.enemy.x >= (window_width - state.enemy.width) || state.enemy.x <= 0){
+                state.enemy_dx *= -1;
+            }
+            state.enemy.y += state.enemy_dy;
+            if(state.enemy.y >= (window_height - state.enemy.height) || state.enemy.y <= 0){
+                state.enemy_dy *= -1;
+            }
 
-        if(box_1.x >= (window_width - box_1.width) || box_1.x <= 0){
-            box_1.dx *= -1;
+            // player movement
+            if(IsKeyDown(KEY_W) && state.player.y >= 0){
+                state.player.y -= 10;
+            }
+            if(IsKeyDown(KEY_S) && state.player.y <= (window_height - state.player.height)){
+                state.player.y += 10;
+            }
+            if(IsKeyDown(KEY_A) && state.player.x >= 0){
+                state.player.x -= 10;
+            }
+            if(IsKeyDown(KEY_D) && state.player.x <= (window_width - state.player.width)){
+                state.player.x += 10;
+            }
+
+            // collision check
+            if(CheckCollisionRecs(state.player, state.coin)){
+                PlaySound(coin_sound);
+                state.score++;
+                state.coin.x = GetRandomValue(50, window_width - 50);
+                state.coin.y = GetRandomValue(50, window_height - 50);
+            }
+
+            // game over check
+            if(CheckCollisionRecs(state.player, state.enemy)){
+                state.gameOver = true;
+            }
+
+            // GPU Draw
+            BeginDrawing();   
+                ClearBackground(RAYWHITE);
+                // draw score
+                DrawText(TextFormat("Score: %d", state.score), 50, 50, 50, GREEN);
+                // draw player
+                //DrawRectangle(state.player.x, state.player.y, state.player.width, state.player.height, BLACK);
+                DrawTexture(player_sprite, state.player.x, state.player.y, WHITE);
+                // draw coin
+                DrawRectangle(state.coin.x, state.coin.y, state.coin.width, state.coin.height, GOLD);
+                // draw enemy
+                DrawRectangle(state.enemy.x, state.enemy.y, state.enemy.width, state.enemy.height, RED);
+            EndDrawing();    
         }
 
-        if(box_1.y >= (window_height - box_1.height) || box_1.y <= 0){
-            box_1.dy *= -1;
+        if(state.gameOver){
+            // restart check
+            if(IsKeyDown(KEY_R)){
+                state = InitGame(&window_width, &window_height);
+            }
+
+            BeginDrawing();
+                ClearBackground(BLACK);
+                DrawText("SKILL ISSUE", window_width/8, window_height/4, 100, RED);
+                DrawText("Press R to restart", window_width/4, window_height/2, 50, RED);
+            EndDrawing();
         }
-
-        if(IsKeyDown(KEY_SPACE)){
-            DrawText("SPACE IS PRESSED", (window_width / 2), (window_height / 2), 20, RED);
-        }
-
-        // draw logic
-        BeginDrawing();
-            ClearBackground(RAYWHITE);
-            DrawRectangle(
-                (int)box_1.x,
-                (int)box_1.y,
-                (int)box_1.width,
-                (int)box_1.height,
-                box_1.color
-            );
-
-            DrawText("Watch it bounce!", 10, 10, 20, LIGHTGRAY);
-        EndDrawing();
     }
-    
 
+    UnloadTexture(player_sprite);
+    UnloadSound(coin_sound);
+    CloseAudioDevice();
     CloseWindow();
     return 0;
 }
